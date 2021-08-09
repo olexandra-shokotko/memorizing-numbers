e2a50dc0c5fa94a6e06228f3c75ceffd52068a75
Olexandra <shokotko.olexandra@gmail.com>
2021-08-09 23:47:15 +0300

    *

--- src/main/java/com/example/memorizingnumbers/controller/MainController.java
+++ src/main/java/com/example/memorizingnumbers/controller/MainController.java
@@ -1,7 +1,13 @@
 package com.example.memorizingnumbers.controller;
 
+import com.example.memorizingnumbers.domain.Role;
+import com.example.memorizingnumbers.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+import java.util.Map;
 
 @Controller
 public class MainController {
@@ -14,4 +20,20 @@ public class MainController {
     public String main() {
         return "main";
     }
+
+//    @PostMapping("/main")
+//    public String storePhone(User user, Map<String, Object> model) {
+//        User userFromDb = userRepo.findByUsername(user.getUsername());
+//
+//        if (userFromDb != null) {
+//            model.put("message", "User already exists!");
+//            return "registration";
+//        }
+//
+//        user.setActive(true);
+//        user.setRoles(Collections.singleton(Role.USER));
+//        userRepo.save(user);
+//
+//        return "redirect:/login";
+//    }
 }
